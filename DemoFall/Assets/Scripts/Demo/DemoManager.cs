16c2ab37312eea3266f0504f039ecbf6c69b755f
Elie H <eli.harfouche@gmail.com>
2019-04-03 17:29:45 -0400

    Add LOD Slider

--- DemoFall/Assets/Scripts/Demo/DemoManager.cs
+++ DemoFall/Assets/Scripts/Demo/DemoManager.cs
@@ -14,17 +14,28 @@ public class DemoManager : MonoBehaviour {
         public Color Albedo;
     }
 
+    [System.Serializable]
+    public struct MaterialStruct
+    {
+        public Material Auto_LOD;
+        public Material Fixed_LOD;
+    }
+
     public GameObject[] Objects;
     private int m_activeObject;
 
-    public Material[] Materials;
+    public MaterialStruct[] Materials;
     private int m_activeMaterial;
 
     public LEANMaps[] Textures;
     private int m_activeTexture;
 
+    private float m_currentLOD;
+    private bool m_autoLOD = true;
+
     public void Start() {
         EnableObject(0);
+        SetAutoLOD(m_autoLOD);
     }
 
     public void EnableObject(int index) {
@@ -40,7 +51,14 @@ public class DemoManager : MonoBehaviour {
     public void SetTexture(int index) {
         if(index < Textures.Length) {
             m_activeTexture = index;
-            Material mat = Materials[m_activeMaterial];
+
+            Material mat = Materials[m_activeMaterial].Auto_LOD;
+            mat.SetTexture("_NormalMap", Textures[m_activeTexture].NormalMap);
+            mat.SetTexture("_Lean1", Textures[m_activeTexture].Lean1);
+            mat.SetTexture("_Lean2", Textures[m_activeTexture].Lean2);
+            mat.SetColor("_Albedo", Textures[m_activeTexture].Albedo);
+
+            mat = Materials[m_activeMaterial].Fixed_LOD;
             mat.SetTexture("_NormalMap", Textures[m_activeTexture].NormalMap);
             mat.SetTexture("_Lean1", Textures[m_activeTexture].Lean1);
             mat.SetTexture("_Lean2", Textures[m_activeTexture].Lean2);
@@ -48,12 +66,27 @@ public class DemoManager : MonoBehaviour {
         }
     }
 
+    private Material GetActiveMaterial() {
+        return this.m_autoLOD ? Materials[m_activeMaterial].Auto_LOD : Materials[m_activeMaterial].Fixed_LOD;
+    }
+
     public void SetMaterial(int index) {
         if (index < Materials.Length) {
             m_activeMaterial = index;
             SetTexture(m_activeTexture);
+            SetLOD(m_currentLOD);
             Renderer renderer = Objects[m_activeObject].GetComponent<Renderer>();
-            renderer.sharedMaterial = Materials[m_activeMaterial];
+            renderer.sharedMaterial = GetActiveMaterial();
         }
+    }
+
+    public void SetLOD(float value) {
+        m_currentLOD = value;
+        Materials[m_activeMaterial].Fixed_LOD.SetFloat("_LOD", value);
+    }
+
+    public void SetAutoLOD(bool value) {
+        m_autoLOD = value;
+        SetMaterial(m_activeMaterial);
     }
 }
---
+++ DemoFall/Assets/Scripts/Demo/LODUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LODUI : MonoBehaviour {
+
+    public Slider LOD_Slider;
+    public Text LOD_Value;
+
+	public void ToggleSlider(bool value) {
+        LOD_Slider.gameObject.SetActive(!value);
+    }
+
+    public void LODValueChange(float value) {
+        LOD_Value.text = value.ToString();
+    }
+}
